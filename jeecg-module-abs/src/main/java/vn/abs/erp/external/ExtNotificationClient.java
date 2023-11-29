d3576f0d1c7181e9d7eb1d303454f133733a1c83
Du Vu <vu.dq@abs.vn>
2023-11-29 17:53:01 +0700

    Updated change logo for ABS

--- jeecg-module-abs/src/main/java/vn/abs/erp/external/ExtNotificationClient.java
+++ jeecg-module-abs/src/main/java/vn/abs/erp/external/ExtNotificationClient.java
@@ -18,6 +18,9 @@ public class ExtNotificationClient {
     @Value("${app.user-agent}")
     private String userAgent;
 
+    @Value("${app.notification-service.access-token}")
+    private String notiBearerAccessToken;
+
     /**
      * Send notification to user, via notification service. No Auth required
      * @param notificationDto notificationDto
@@ -26,12 +29,11 @@ public class ExtNotificationClient {
     public NotificationResult sendNotification(NotificationDto notificationDto) {
         OkHttpClient client = AbsHttpClient.getClient();
         MediaType mediaType = MediaType.parse("application/json");
-        // create request body from notificationDto
         RequestBody body = RequestBody.create(notificationDto.toJsonString(), mediaType);
         Request request = new Request.Builder()
                 .method("POST", body)
                 .addHeader("User-Agent", userAgent)
-                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdG9ja0FjY291bnQiOm51bGwsInN1YkFjY291bnRzIjpudWxsLCJzdWIiOiJTVVBFUl9VU0VSIiwicm9sZXMiOiJST0xFX1NVUEVSX1VTRVIiLCJpZCI6IlNVUEVSX1VTRVIiLCJ0eXBlIjoiU1VQRVJfVVNFUiIsImV4cCI6MjY1NDU3NTc5NCwiaWF0IjoxNjU0NTc1Nzk0fQ.XBMZVlj-NbMoi5VdEudVsRr3cVD-Op-mByTJ-0NMiun5IPcPZhbrdBwtrePhdPtx5eprEZgZcWoqQIGUbn37pw")
+                .addHeader("Authorization", notiBearerAccessToken)
                 .url(notificationUrl)
                 .build();
 
